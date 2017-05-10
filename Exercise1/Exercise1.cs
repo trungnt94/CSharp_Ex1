019dfe4c91148b7f8574df09af0e07e0c993ea36
TrungNT94 <nguyentrung.cp12@gmail.com>
2017-05-11 02:03:43 +0700

    push source

---
+++ Exercise1/Exercise1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Exercise1
+{
+  
+    class Exercise1
+    {
+        List<User> lst = new List<User>();
+        public void  inputList()
+        {            
+            User us = new User();
+            Console.WriteLine("Nhap username: ");
+            us.username = Console.ReadLine();
+            Console.WriteLine("Nhap password: ");
+            us.password = Console.ReadLine();
+            lst.Add(us);
+        }
+
+        public void showList()
+        {
+            foreach (User i in lst)
+            {
+                Console.WriteLine("Username: "+i.username);
+                Console.WriteLine("Password: "+i.password);
+            }        
+        }
+
+        public static void menu()
+        {
+            Console.WriteLine("1. Nhap user.");
+            Console.WriteLine("2. Hien thi user.");
+            Console.WriteLine("3. Thoat");
+        }
+        static void Main(string[] args)
+        {
+            Exercise1 ex = new Exercise1();                   
+            int choice;
+            do
+            {
+                menu();
+                string inputStr = Console.ReadLine();
+                choice = Int32.Parse(inputStr);
+                switch (choice)
+                {
+                    case 1:
+                        ex.inputList();
+                        break;
+                    case 2:
+                        ex.showList();
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong");
+                        break;
+                }
+                Console.ReadLine();
+            } while (choice != 3);
+        }
+    }
+}
---
+++ Exercise1/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercise1
+{
+    class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
